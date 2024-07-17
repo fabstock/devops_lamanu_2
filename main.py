91d7d57476015f6bc9e4957edffd86ea3bd6c78a
Fab Stock <fabstock@stock.eu.org>
2024-07-17 15:49:21 +0200

    correction: commenterexercises 3 et 4

--- main.py
+++ main.py
@@ -21,24 +21,24 @@ number = int(number)
 
 ### Exo 3
 
-my_list = ["Python", "JavaScript", "Java", "PHP", "C++", "Cobol"]
+#my_list = ["Python", "JavaScript", "Java", "PHP", "C++", "Cobol"]
 
-print("With for loop")
-for language in my_list:
-   print(language)
+#print("With for loop")
+#for language in my_list:
+#   print(language)
 
-i = 0
+#i = 0
 
-print("With while loop")
-while i < len(my_list):
-   print(my_list[i])
-   i += 1
+#print("With while loop")
+#while i < len(my_list):
+#   print(my_list[i])
+#   i += 1
 
 ### Exo 4
 
-my_list = []
+#my_list = []
 
-for i in range(1, 21):
-   number = my_list.append(int(input(f"[{i}/20] Entrer le nombre {i} : ")))
+#for i in range(1, 21):
+#   number = my_list.append(int(input(f"[{i}/20] Entrer le nombre {i} : ")))
 
-print(f"Le plus grand nombre est {max(my_list)} en position {my_list.index(max(my_list)) + 1} ")
+#print(f"Le plus grand nombre est {max(my_list)} en position {my_list.index(max(my_list)) + 1} ")
