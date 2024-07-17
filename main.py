51eddf7b0c28dff2d0082bff0b580698a74864e8
Fab Stock <fabstock@stock.eu.org>
2024-07-17 14:48:58 +0200

    first # remove

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-#### Exo 1
 
-# number = int(input("Enter a number: "))
+#test  
+### Exo 1
 
-# number = int(number)
+number = int(input("Enter a number: "))
 
-#if number < 10:
-#    print(f"Plus grand !")
+number = int(number)
 
-#if number > 20:
-#    print(f"Plus petit !")
 
-# number_list = []
+
+#### Exo 2
+# while True:
+    # number = int(input("Enter a number: "))
+    # if 0 == number:
+        # break
+
+    # number_list.append(int(number))
+
+# print(f"La somme est {sum(number_list)}")
+
+
+### Exo 3
+
+my_list = ["Python", "JavaScript", "Java", "PHP", "C++", "Cobol"]
+
+print("With for loop")
+for language in my_list:
+   print(language)
+
+i = 0
+
+print("With while loop")
+while i < len(my_list):
+   print(my_list[i])
+   i += 1
+
+### Exo 4
+
+my_list = []
+
+for i in range(1, 21):
+   number = my_list.append(int(input(f"[{i}/20] Entrer le nombre {i} : ")))
+
+print(f"Le plus grand nombre est {max(my_list)} en position {my_list.index(max(my_list)) + 1} ")
