a0c86f0d9aabb32fd8169a9f567384db9098331a
Thomas CARBUCCIA <thomas.carbuccia@proxigital.fr>
2024-07-17 14:44:23 +0200

    Init

---
+++ main.py
@@ -0,0 +1,13 @@
+#### Exo 1
+
+# number = int(input("Enter a number: "))
+
+# number = int(number)
+
+#if number < 10:
+#    print(f"Plus grand !")
+
+#if number > 20:
+#    print(f"Plus petit !")
+
+# number_list = []
