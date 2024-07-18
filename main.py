9ca435b985e897c15c5c3761be8e5fda889c6814
Fab Stock <fabstock@stock.eu.org>
2024-07-18 10:24:42 +0200

    test

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-#### Exo 1
+### Exo 1
+import random
+random.seed(1)
 
-# number = int(input("Enter a number: "))
+num = random.random()
+print(num)
 
-# number = int(number)
+#number = int(input("Enter a number: "))
+number = num * 10
 
-#if number < 10:
-#    print(f"Plus grand !")
+if number < 10:
+    print(f"Plus grand !")
 
-#if number > 20:
-#    print(f"Plus petit !")
+if number > 20:
+    print(f"Plus petit !")
 
-# number_list = []
+number_list = []
